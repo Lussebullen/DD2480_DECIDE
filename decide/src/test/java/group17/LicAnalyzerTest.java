887a00fcefd0f81713e0905f3dd4678b3fb5b728
Omar Askar Vergara <omaraskar@outlook.com>
2024-01-27 14:51:27 +0100

    feat/ Add positive and negative unit test for lic0
    Two seperate unit test for positive behaviour and negative behaviour if
    invalid input. Currently fails until `lic0()` is implemented.

--- decide/src/test/java/group17/LicAnalyzerTest.java
+++ decide/src/test/java/group17/LicAnalyzerTest.java
@@ -4,20 +4,47 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.Exception;
 
 public class LicAnalyzerTest {
     
     private static InputHandler input;
+    private static LicAnalyzer licAnalyzer;
 
     @BeforeEach
     public void setUp() {
         input = new InputHandler("sampleData.json");
+        licAnalyzer = new LicAnalyzer();
     }
 
     @Test
-    public void lic0Test() {
-        assertTrue(true);
+    public void lic0ReturnsTrueIfConsecutivePointsGreaterThanLENGTH1Exists() {
+        // Arrange
+        input.NUMPOINTS = 2;
+        input.X_COORD[0] = 1.0;
+        input.Y_COORD[0] = 1.0;
+
+        input.X_COORD[1] = 10.0;
+        input.Y_COORD[1] = 10.0;
+
+        try {
+            //Act
+            boolean signal = licAnalyzer.lic0(input);
+            //Assert
+            assertTrue(signal);
+        } catch (Exception e) {}
+    }
+
+    @Test
+    public void lic0ThrowsErrorIfLENGTH1IsInvalidByBeingLowerThanError() {
+        //Arrange
+        input.LENGTH1 = -0.00000001;
+
+        //Act, Assert
+        assertThrows(Exception.class, () -> licAnalyzer.lic0(input));
     }
 
     @Test
