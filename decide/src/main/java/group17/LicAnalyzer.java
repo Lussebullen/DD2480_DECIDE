6df1b878613eefe7b9550b80d291dddefca38cf1
Omar Askar Vergara <omaraskar@outlook.com>
2024-01-28 21:10:48 +0100

    refactor/ Modularize lic10 to search in both orders
        Lic10 now has a private helper method that takes the required
        amount of intervening points between first two points as first
        argument and required amount of between second two points as
        second argument and returns true if such a set exists. This
        way we can search for points seperated in both orders, i.e:
    
        <point1> F_PTS <point2> E_PTS <point3>
    
        or
    
        <point1> E_PTS <point2> F_PTS <point3>

--- decide/src/main/java/group17/LicAnalyzer.java
+++ decide/src/main/java/group17/LicAnalyzer.java
@@ -42,8 +42,52 @@ public class LicAnalyzer {
         return false;
     }
 
-    public boolean lic10(InputHandler input) {
+    private boolean helperLic10ThreePointsSeperateBy(int quantity1, int quantity2, InputHandler input)
+    {
+        for (int i = quantity1 + quantity2 + 2; i < input.NUMPOINTS; ++i) {
+            int vertex3 = i;
+            int vertex2 = i - quantity1 - 1;
+            int vertex1 = i - quantity1 - quantity2 - 2;
+
+            //Calculate triangle area from three points using shoelace formula
+            double xCoordinate1 = input.X_COORD[vertex1];
+            double yCoordinate1 = input.Y_COORD[vertex1];
+
+            double xCoordinate2 = input.X_COORD[vertex2];
+            double yCoordinate2 = input.Y_COORD[vertex2];
+
+            double xCoordinate3 = input.X_COORD[vertex3];
+            double yCoordinate3 = input.Y_COORD[vertex3];
+
+            double triangleArea = 0.5 * Math.abs( (xCoordinate1 - xCoordinate3) * (yCoordinate2 - yCoordinate1)
+                                                 -(xCoordinate1 - xCoordinate2) * (yCoordinate3 - yCoordinate1)
+                                                );
+            if (triangleArea > input.AREA1) {
+                return true;
+            }
+        }
+
         return false;
+    }
+
+    public boolean lic10(InputHandler input) throws Exception {
+        if (input.E_PTS < 1 || input.F_PTS < 1) {
+            throw new Exception("Exception thrown from: LIC 10. E_PTS < 1 or F_PTS < 1.");
+        } else if (input.NUMPOINTS < input.E_PTS + input.F_PTS + 3) {
+            throw new Exception("Exception thrown from: LIC 10. NUMPOINTS is lower than E_PTS + F_PTS + 3.");
+        }
+
+        if (input.NUMPOINTS < 5) {
+            return false;
+        }
+
+        boolean pointsFound = helperLic10ThreePointsSeperateBy(input.E_PTS, input.F_PTS, input);
+        if (!pointsFound) {
+            // Reverse order
+            pointsFound = helperLic10ThreePointsSeperateBy(input.F_PTS, input.E_PTS, input);
+        }
+        return pointsFound;
+
     }
 
     public boolean lic11(InputHandler input) {
