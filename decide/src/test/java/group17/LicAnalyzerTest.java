5c9c5c38c0c9bc639b84dfc59ea72eab546ef2a4
Omar Askar Vergara <omaraskar@outlook.com>
2024-01-28 21:06:13 +0100

    feat/ Add positive unit test for lic10
        `Lic10` now has a positive unit test that tests that three
        vertices can be found that are seperated by E_PTS and F_PTS
        respecticly with an area greater than AREA1.

--- decide/src/test/java/group17/LicAnalyzerTest.java
+++ decide/src/test/java/group17/LicAnalyzerTest.java
@@ -4,16 +4,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.Exception;
 
 public class LicAnalyzerTest {
     
     private static InputHandler input;
+    private static LicAnalyzer licAnalyzer;
 
     @BeforeEach
     public void setUp() {
         input = new InputHandler("sampleData.json");
+        licAnalyzer = new LicAnalyzer();
     }
+
 
     @Test
     public void lic0Test() {
@@ -66,8 +72,25 @@ public class LicAnalyzerTest {
     }
 
     @Test
-    public void lic10Test() {
-        assertTrue(true);
+    public void lic10IsTrueIfThreePointsSeperatedByTwoAndFourPointsExistsWithAreaGreaterThan49() {
+        //Arrange
+        double[] arrangedX_COORD = {1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 11.0};
+        double[] arrangedY_COORD = {1.0, 0.0, 0.0, 11.0, 0.0, 0.0, 0.0, 0.0, 1.0};
+
+        input.X_COORD = arrangedX_COORD;
+        input.Y_COORD = arrangedY_COORD;
+        input.NUMPOINTS = 9;
+        input.E_PTS = 2;
+        input.F_PTS = 4;
+        input.AREA1 = 49.0;
+
+        try {
+            //Act
+            boolean signal = licAnalyzer.lic10(input);
+
+            // Assert
+            assertTrue(signal);
+        } catch (Exception e) {}
     }
 
     @Test
