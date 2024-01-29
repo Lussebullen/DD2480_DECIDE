ecf28d061f47987aa98aceaa49a4236768f9c721
Omar Askar Vergara <omaraskar@outlook.com>
2024-01-29 18:04:38 +0100

    docs/ Add documentation for LIC5

--- decide/src/main/java/group17/LicAnalyzer.java
+++ decide/src/main/java/group17/LicAnalyzer.java
@@ -22,6 +22,15 @@ public class LicAnalyzer {
         return false;
     }
 
+     /*
+     * Evaluates if a set of two consecutive points exists where latter point has a lower value on its x-coordinate,
+     * more specifically, if it exists points such that X[i] < X[j], where j = i - 1 and X is a container with holding
+     * all points' x-coordinates.
+     *
+     * @param  input  object whose members hold data for the problem; relevant 
+                      fields for lic5 are: NUMPOINTS, X_COORD, Y_COORD
+     * @return        true if set exists, false otherwise
+     */
     public boolean lic5(InputHandler input) throws IllegalArgumentException {
         if (input.NUMPOINTS < 2 || input.NUMPOINTS > 100) {
             throw new IllegalArgumentException("Exception thrown from: LIC 0. Reason: NUMPOINTS outside range [2, 100].");
