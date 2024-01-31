9c48f01278c875e069ba1f1ef7e03ee389680a96
empazi <emir.catir@gmail.com>
2024-01-31 11:45:45 +0100

    Removed local path from main

--- decide/src/main/java/group17/App.java
+++ decide/src/main/java/group17/App.java
@@ -92,7 +92,7 @@ public class App
 
     public static void main( String[] args ) {
         try {
-            InputHandler input = new InputHandler("./decide/sampleData.json");
+            InputHandler input = new InputHandler("");
             App missileSystem = new App();
             missileSystem.decide(input);
         }
